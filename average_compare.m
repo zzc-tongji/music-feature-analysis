56aa1ef9478d704eb690607344a8647c28b98808
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2016-04-20 10:25:27 +0800

    Add a function for comparing the mean of all features in different music.

---
+++ average_compare.m
@@ -0,0 +1,20 @@
+function result = average_compare(m1, m2)
+%COMPARE MEAN of DATA
+%
+%   result = average_compare(m1, m2)
+%       Parameter 'm1' and 'm2' must be must be two-dimensional matrixes.
+%       If values in m1 is bigger, the return value is positive.
+
+% check paramete
+s1 = size(m1);
+s2 = size(m2);
+if length(s1) ~= 2 || length(s2) ~= 2
+    error('Parameter ''m1'' and ''m2'' must be must be two-dimensional matrixes.');
+end
+
+% calculate
+m1_average = mean(m1);
+m2_average = mean(m2);
+result = (m1_average - m2_average) ./ ((m1_average + m2_average) ./ 2);
+
+end
