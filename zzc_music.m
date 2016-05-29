f936fbcfbd3ec43017c807cbe91f243d04abadda
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2016-05-29 08:39:45 +0800

    Merge all functions called by function 'zzc_music' to file 'zzc_music.m'. Make sure they cannot be misused outside.

--- zzc_music.m
+++ zzc_music.m
@@ -140,3 +140,253 @@ title = {'spectral_centroid', 'spectral_entropy', 'spectral_flux', ...
 disp 'All Finished.'
 
 end
+
+function [sound_intensity, sample_rate] = audio_sound_intensity(wav_file, method)
+%CACULATE SOUND INTENSITY of AUDIO WAVE
+%
+%   [sound_intensity, sample_rate] = audio_sound_intensity(wav_file)
+%       Use 'linear' as the default interpolation method in tne end.
+%       Return value 'sound_intensity' is a matrix with 2 columns:
+%           1st column - sound intensity
+%           2nd column - time (s)
+%
+%   sound_intensity = audio_sound_intensity(wav_file, method)
+%       Parameter 'method' indicates interpolation method in the end,
+%       it must be one of them:
+%          'nearest'  - nearest neighbor interpolation
+%          'linear'   - linear interpolation
+%          'spline'   - piecewise cubic spline interpolation (SPLINE)
+%          'pchip'    - shape-preserving piecewise cubic interpolation
+%          'cubic'    - same as 'pchip'
+%          'v5cubic'  - the cubic interpolation from MATLAB 5, which does not
+%                       extrapolate and uses 'spline' if X is not equally spaced.
+
+% check parameter
+if ~ischar(wav_file) || strcmp(wav_file, '')
+	error('Parameter 1 must be a non-empty string.');
+end
+if (nargin == 1)
+elseif (nargin == 2)
+    if ~ischar(method) || (~ strcmp(method, 'nearest') && ~ strcmp(method, 'linear') && ~ strcmp(method, 'spline') && ~ strcmp(method, 'pchip') && ~ strcmp(method, 'cubic') && ~ strcmp(method, 'v5cubic'))
+        error('Parameter 2 must be a string which must be one of them: ''linear'', ''spline'', ''pchip'', ''cubic'' or ''v5cubic''.');
+    end
+else
+    error('There are too much parameters.');
+end
+
+% read .wav file
+[wave, sample_rate, ~] = wavread(wav_file);
+
+% Sound intensity, in my opinion, is the linking route of all max-extremum point.
+
+% difference
+loop_end_index = length(wave);
+difference = zeros(loop_end_index, 1);
+difference(1) = 0;
+for index = 2 : loop_end_index
+    difference(index) = wave(index) - wave(index - 1);
+end
+
+% get all all max-extremum point
+max_extremum = zeros(loop_end_index, 1);
+max_extremum_length = 0;
+for index = 1 : loop_end_index - 1
+    if difference(index) > 0 && difference(index + 1) < 0 && wave(index) > 0
+        max_extremum_length = max_extremum_length + 1;
+        max_extremum(max_extremum_length, 1) = wave(index);
+        max_extremum(max_extremum_length, 2) = index;
+    end
+end
+max_extremum = max_extremum(1 : max_extremum_length, :); % intercept
+
+% interpolate
+sound_intensity = zeros(length(wave), 2);
+for index = 1 : loop_end_index
+    sound_intensity(index, 2) = index;
+end
+if(nargin == 1)
+    sound_intensity(:, 1) = interp1(max_extremum(:, 2), max_extremum(:, 1), sound_intensity(:, 2));
+else
+    sound_intensity(:, 1) = interp1(max_extremum(:, 2), max_extremum(:, 1), sound_intensity(:, 2), method);
+end
+
+% check value
+for index = 1 : loop_end_index
+    if isnan(sound_intensity(index, 1)) == 1 || sound_intensity(index, 1) < 0
+        sound_intensity(index, 1) = 0;
+    end
+end
+
+% set time
+sound_intensity(:, 2) = sound_intensity(:, 2) / sample_rate;
+
+end
+
+function power = audio_power(wav_file, frame_s, non_overlap_s)
+%CACULATE POWER of AUDIO WAVE
+%
+%   power_result = audio_power(wav_file, frame_s, non_overlap_s)
+%       Use 'linear' as the default interpolation method in tne end.
+%       Return value 'power' is a matrix with 2 columns:
+%           1st column - power
+%           2nd column - time (s)
+
+% check parameter
+if ~ischar(wav_file) || strcmp(wav_file, '')
+	error('Parameter 1 must be a non-empty string.');
+end
+if ~isfloat(frame_s)
+    error('Parameter 2 must be a double.');
+end
+if ~isfloat(non_overlap_s)
+    error('Parameter 3 must be a double.');
+end
+if frame_s < non_overlap_s
+    error('Parameter 2 should be no less than parameter 3.');
+end
+
+% read .wav file
+[wave, sample_rate, ~] = wavread(wav_file);
+
+% transform from second to point
+temp = sample_rate * frame_s;
+frame_point = floor(temp);
+if frame_point ~= temp
+    error('Number of sampling points in a frame is not an integer.');
+end
+temp = sample_rate * non_overlap_s;
+non_overlap_point = floor(temp);
+if non_overlap_point ~= temp
+    error('Number of sampling points in non-overlaping part of a frame is not an integer.');
+end
+
+frame_number = floor((length(wave) - (frame_point - non_overlap_point)) / non_overlap_point);
+
+% calculate power
+power = zeros(frame_number, 2);
+for index = 1 : 1 : frame_number
+    select_end = index * non_overlap_point + (frame_point - non_overlap_point);
+    select_begin = select_end - frame_point + 1;
+    % This is the core formula.
+    power(index, 1) = sum(wave(select_begin : select_end).^ 2) / frame_s;
+end
+
+% set time
+for index = 1 : frame_number
+    power(index, 2) = frame_s / 2 + (index - 1) * non_overlap_s;
+end
+
+end
+
+function sharpness = audio_sharpness(power)
+%CACULATE SHARPNESS of AUDIO WAVE
+%
+%   sharpness = audio_sharpness(power)
+%       Parameter 'power' must be the result of function 'audio_power'.
+%       Return value 'sharpness' is a matrix with 2 columns:
+%           1st column - sharpness
+%           2nd column - time (s)
+
+% check parameter
+if length(size(power)) ~= 2
+    error('Parameter must be a two-dimensional matrix.');
+end
+
+% calculate sharpness
+loop_end_index = length(power);
+sharpness = zeros(loop_end_index, 2);
+for index = 2 : loop_end_index
+    % Sharpness, defined as the mean positive first derivative of the waveform power.
+    % Because of the descrete data and the short time interval,
+    % interative differential method is used here to replace the derivation.
+    sharpness(index, 1) = (power(index, 1) - power(index - 1, 1)) / ...
+                            (power(index, 2) - power(index - 1, 2));
+end
+sharpness(1, 1) = sharpness(2, 1);
+sharpness(:, 2) = power(:, 2);
+
+end
+
+function result = sps_rebuild(data, frame_length_s, non_overlap_length_s)
+%REBUILD of SOUND INTENSITY, POWER and SHARPNESS by MEAN
+%
+%   result = sps_reconstruct(data, frame_length_s, non_overlap_s)
+%       Parameter 'data' must be the result of function 'audio_sound_intensity',
+%           function 'audio_power' or function 'audio_sharpness'.
+%       Return value 'result' is a column vector, no longer include time information.
+
+% check parameter
+if ~ismatrix(data)
+    error('Parameter 1 must be a matrix');
+end
+if ~isfloat(frame_length_s)
+    error('Parameter 2 must be a double.');
+end
+if ~isfloat(non_overlap_length_s)
+    error('Parameter 3 must be a double.');
+end
+if frame_length_s < non_overlap_length_s
+    error('Parameter 2 should be no less than parameter 3.');
+end
+
+% rebuild
+next_time = frame_length_s;
+selected_begin = 1;
+result = zeros(ceil((ceil(data(end, 2)) - (frame_length_s - non_overlap_length_s)) / non_overlap_length_s), 1);
+index_1 = 1;
+for index_2 = 1 : 1 : size(data, 1)
+    if data(index_2, 2) > next_time
+        % locate
+        selected_end = index_2 - 1;
+        % caculate mean
+        result(index_1) = mean(data(selected_begin : selected_end, 1));
+        index_1 = index_1 + 1;
+        % locate
+        selected_begin = index_2;
+        next_time = next_time + non_overlap_length_s;
+        % no data left
+        if index_2 == size(data, 1)
+            break;
+        end
+    end
+    % the rest of data
+    if index_2 == size(data, 1)
+        selected_end = index_2;
+        result(index_1) = mean(data(selected_begin : selected_end, 1));
+    end
+end
+result = result(1 : index_1);
+
+end
+
+function fluctuation_centroid = audio_fluctuation_centroid(d_fluc)
+%CACULATE FLUCTUATION CENTROID of A FRAME
+%
+% This function is written originally by Wang Deqing <deqing.wang@foxmail.com>.
+% Some modifications have been added.
+
+fram_num = size(d_fluc, 2);
+window_length = size(d_fluc, 1);
+fluctuation_centroid = zeros(1, fram_num);
+m = ((10 / (window_length - 1)) * (0 : window_length - 1))';
+for index = 1 : fram_num
+    fram = d_fluc(:, index);
+    fluctuation_centroid(index) = sum(m .* fram) / (sum(fram) + eps);
+end
+
+end
+
+function fluctuation_entropy = audio_fluctuation_entropy(d_fluc)
+%CACULATE FLUCTUATION ENTROPY of A FRAME
+%
+% This function is written originally by Wang Deqing <deqing.wang@foxmail.com>.
+% Some modifications have been added.
+
+d_fluc_abs = abs(d_fluc);
+summat = repmat(sum(d_fluc_abs) + repmat(eps, ...
+    [1 size(d_fluc_abs, 2) size(d_fluc_abs, 3) size(d_fluc_abs, 4)]), ...
+    [size(d_fluc_abs, 1) 1 1 1]);
+d_fluc_abs = d_fluc_abs ./ summat;
+fluctuation_entropy = - sum(d_fluc_abs .* log(d_fluc_abs + eps)) ./ log(size(d_fluc_abs, 1));
+
+end
